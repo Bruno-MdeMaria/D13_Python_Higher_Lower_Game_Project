f2dfa3bbc1888682fa2dae38c79b274e3c4b9253
Pricila santos <brunomartinsdemaria@gmail.com>
2022-08-11 01:01:04 -0300

    ACERTO DE FUNCAO COMPARAR
    BUG DE NOME DA KEY

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from secrets import choice
 from art import logo, vs
 from game_data import data
 import random
@@ -12,23 +11,24 @@ def rodada(comp_a, comp_b):
     print(f"COM B: {comp_b['name']}, um(a) {comp_b['description']} do(a) {comp_b['country']}.")
 
 def comparar(comp_a, comp_b, resposta):
-    
+    a_dict = int(comp_a['follower_count'])
+    b_dict = int(comp_b['follower_count'])
+   
     placar = 0
     if resposta == "a" and a_dict > b_dict:
         placar += 1
+        print(placar) #TEMP
     elif resposta == "b" and a_dict < b_dict:
         placar += 1
+        print(placar)#TEMP
     else: print(f"Você errou! Seu placar é de {placar}")
     
 
 
 comp_a = escolha()
 comp_b = escolha()
-a_dict = comp_a['follower_count']
-b_dict = comp_b['follower_cout']
-
-print(a_dict['follower_count'])
+print(comp_b)
 print(comp_a)
 rodada(comp_a, comp_b)
 resposta = input("Quem tem mais seguidores no Instagram? Digite 'A' ou 'B': ").lower()
-#comparar(comp_a, comp_b, resposta)
+comparar(comp_a, comp_b, resposta)

9564274208065f6408ea6ad5f84b7310301a3bf0
Pricila santos <brunomartinsdemaria@gmail.com>
2022-08-11 00:48:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,24 @@ def rodada(comp_a, comp_b):
     print(vs)
     print(f"COM B: {comp_b['name']}, um(a) {comp_b['description']} do(a) {comp_b['country']}.")
 
+def comparar(comp_a, comp_b, resposta):
+    
+    placar = 0
+    if resposta == "a" and a_dict > b_dict:
+        placar += 1
+    elif resposta == "b" and a_dict < b_dict:
+        placar += 1
+    else: print(f"Você errou! Seu placar é de {placar}")
+    
+
 
 comp_a = escolha()
 comp_b = escolha()
+a_dict = comp_a['follower_count']
+b_dict = comp_b['follower_cout']
+
+print(a_dict['follower_count'])
+print(comp_a)
 rodada(comp_a, comp_b)
 resposta = input("Quem tem mais seguidores no Instagram? Digite 'A' ou 'B': ").lower()
+#comparar(comp_a, comp_b, resposta)
